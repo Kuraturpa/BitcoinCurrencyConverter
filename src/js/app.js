72d5ae739cb0efd834005c97cea558e0c1f4337b
Joonas Ruotsalainen <joo.ruot@gmail.com>
2013-03-31 22:42:08 +0700

    Added currency to BTC conversion.

--- src/js/app.js
+++ src/js/app.js
@@ -8,16 +8,16 @@ angular.module('calc', []).factory('apiService', function($http) {
 	api.getRates = function(callback, error) {
 		console.log('get rates');
 		$http.get(api.url).success(callback).error(error);
-		//callback();
 	};
 
 	return api;
 });
 
 angular.module('app', []).controller('uiController', function($scope, apiService) {
-	$scope.rates = {};
-	$scope.currencies = [];
+	$scope.rates = [];
 	$scope.bitcoinAmount = 1;
+	$scope.cur = null;
+	$scope.curAmount = 0;
 	$scope.init = function(param) {
 		console.log('app init');
 		$scope.refreshRates();
@@ -26,10 +26,15 @@ angular.module('app', []).controller('uiController', function($scope, apiService
 	$scope.refreshRates = function() {
 		console.log('refresh rates');
 		apiService.getRates(function(rates) {
-			console.log('rates recieved');
-			console.log(rates);
-			$scope.rates = rates;
-			$scope.currencies = Object.keys(rates);
+			for(var i=0;i<Object.keys(rates).length;i++) {
+				var rate = {};
+				rate.name = Object.keys(rates)[i];
+				rate.last = rates[Object.keys(rates)[i]].last;
+				rate.symbol = rates[Object.keys(rates)[i]].symbol;
+				$scope.rates.push(rate);
+			}
+			if($scope.cur == null)
+				$scope.cur = $scope.rates[0];
 		},
 		function(data){
 			console.log("error");
@@ -37,12 +42,28 @@ angular.module('app', []).controller('uiController', function($scope, apiService
 		});
 	};
 
-	$scope.getCurrencyInBitcoins = function(amount) {
+	$scope.convertToCurrency = function(amount) {
 		$scope.bitcoinAmount = parseFloat($scope.bitcoinAmount);
 		if($scope.bitcoinAmount >= 0 && typeof($scope.bitcoinAmount) == "number" && !isNaN($scope.bitcoinAmount) && $scope.bitcoinAmount != "")
 			return amount * $scope.bitcoinAmount;
 		return 0;
 
+	};
+
+	$scope.convertToBitcoin = function(amount) {
+
+	};
+
+	$scope.setCur = function(cur) {
+		$scope.cur = cur.rate;
+	};
+
+	$scope.btcAmountChanged = function() {
+		$scope.curAmount = $scope.convertToCurrency($scope.cur.last);
+	};
+
+	$scope.curAmountChanged = function() {
+		$scope.bitcoinAmount = $scope.curAmount / $scope.cur.last;
 	};
 
 });
